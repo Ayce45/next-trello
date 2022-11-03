c462d9b91aaf86ad124610182f389482dfabff66
Ayce45 <evan.j@hotmail.fr>
2022-11-03 18:00:21 +0100

    🚧 pixel perfect layout

---
+++ components/organisms/Header.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import trello from 'assets/trello.png'
+
+const Header = () => (
+    <header>
+        <nav className="flex justify-center items-center bg-[#111111] w-full h-[40px]">
+            <div className="h-[30px] flex items-center justify-center">
+                <Image src={trello} alt="Trello Logo" className="cursor-pointer opacity-50 hover:opacity-70 h-full w-full" />
+            </div>
+        </nav>
+    </header>
+);
+
+export default Header
