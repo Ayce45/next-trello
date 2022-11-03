818ce2c179821904aae7f5ec7ba1c0ec897b7b06
Ayce45 <evan.j@hotmail.fr>
2022-11-03 18:04:48 +0100

    🐛 fix error image optimization

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  exportPathMap: async function (
-      defaultPathMap,
-      { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
+    reactStrictMode: true,
+    images: {
+        unoptimized: true
     }
-  },
 }
